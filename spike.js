52b084d2cb1c37111cc05191008e656d319a8292
Woxstone <nachomerling@gmail.com>
2022-03-22 11:00:54 +0100

    nada

--- spike.js
+++ spike.js
@@ -1,9 +1,10 @@
 import ffmpeg from "fluent-ffmpeg";
-// const ffmpeg = require("fluent-ffmpeg");
-var vosk = require('vosk')
-
-const fs = require("fs");
-const { spawn } = require("child_process");
+// var vosk = require('vosk');
+import vosk from "vosk";
+import fs from 'fs';
+// const fs = require("fs");
+import { spawn } from "child_process"
+// const { spawn } = require("child_process");
 
 
 const m4afilename = './default_audio.m4a';
@@ -13,13 +14,10 @@ let converttowav = async function (m4afilename, outputFile) {
     let command = ffmpeg(m4afilename).save(outputFile);
 };
 
-
-
 converttowav(m4afilename, outputFile).then(speechrecog(outputFile));
 
 
 function speechrecog(FILE_NAME) {
-
 
     const MODEL_PATH = "./vosk-model-small-es-0.22"
     const SAMPLE_RATE = 16000
@@ -36,6 +34,7 @@ function speechrecog(FILE_NAME) {
     vosk.setLogLevel(0);
     const model = new vosk.Model(MODEL_PATH);
     const rec = new vosk.Recognizer({ model: model, sampleRate: SAMPLE_RATE });
+    const settings = rec.setWords(true);
 
     const ffmpeg_run = spawn('ffmpeg', ['-loglevel', 'quiet', '-i', FILE_NAME,
         '-ar', String(SAMPLE_RATE), '-ac', '1',
@@ -44,11 +43,12 @@ function speechrecog(FILE_NAME) {
     ffmpeg_run.stdout.on('data', (stdout) => {
         if (rec.acceptWaveform(stdout)) {
             console.log(rec.result());
-            // console.log(rec.setWords(true))
         }
-        else
-            console.log(rec.partialResult());
+        else { 
+            // console.log(rec.partialResult()); 
+        }
         console.log(rec.finalResult());
-        // console.log(rec.setWords(false))
     });
 }
+
+export { speechrecog };
---
+++ test/spike.test.js
@@ -0,0 +1,15 @@
+// import { speechrecog } from '../spike.js';
+
+// describe('Test SpeechRecognition', () => {
+//     it('the return have de conf and the word', () => {
+//         const expected = [{ conf: 0.977247, end: 1.571625, start: 1.151625, word: 'hola' }];
+
+//         const result = speechrecog('./default_audio.wav')
+
+//         expect(result).toEqual(expected);
+//     })
+// })
+
+it('dummy', () => {
+    expect(true).toEqual(true);
+})
